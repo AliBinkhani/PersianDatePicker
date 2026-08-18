1125a121320032a0655fbc07f7b96f119ee52da5
AliBi <gublin.97@gmail.com>
2026-08-18 17:21:52 +0330

    Rename project to PersianDatePicker

--- persiandatepicker/src/androidTest/java/com/hooshkar/persiandatepicker/ExampleInstrumentedTest.kt
+++ persiandatepicker/src/androidTest/java/com/hooshkar/persiandatepicker/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hooshkar.jalalidatepicker.test", appContext.packageName)
+        assertEquals("com.hooshkar.persiandatepicker.test", appContext.packageName)
     }
 }
